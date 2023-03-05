cb1afa7c9def36a33d6075d576fb91ecb5745dca
Victor Okechukwu <VicOke360@gmail.com>
2023-03-05 07:46:48 +0100

    More updates made

--- clockTime.js
+++ clockTime.js
@@ -58,6 +58,12 @@ let dotRotate = 0
 function dotss() {
     for (let i = 0; i < dots.length; i++) {
         // dots[i].innerHTML = "<p></p>"
+        if (i > 0) {
+            dotRotate += 6
+            dots[i].style.cssText = `transform: rotate(${dotRotate}deg)`
+            // console.log(dotRotate)
+            // dots[i].s
+        }
         if (i !== 0 && i !== 15 && i !== 30 && i !== 45) {
             // dots[i].innerHTML = "<p></p>"
             let dotPara = document.createElement('P')
@@ -70,12 +76,6 @@ function dotss() {
         // console.log(dots[i])
         // console.log(dotarr)
         
-        if (i > 0) {
-            dotRotate += 6
-            dots[i].style.cssText = `transform: rotate(${dotRotate}deg)`
-            // console.log(dotRotate)
-            // dots[i].s
-        }
         else if (i == 0) {
             let dotRotate = 0
             dots[i].style.cssText = `transform: rotate(${dotRotate}deg)`
@@ -86,32 +86,47 @@ function dotss() {
             dotPara.classList.add("dotPara")
         }
         else if (i == 15) {
-            let dotRotate = 190
+            let dotRotate = 90
             dots[i].style.cssText = `transform: rotate(${dotRotate}deg)`
             let dotPara1 = document.createElement('P')
+            dotPara1.style.cssText = `
+            transform: rotate(270deg);
+            right: 7px;
+            top: 1px;
+            `
             let dotText1 = document.createTextNode('3')
             dotPara1.appendChild(dotText1)
             dots[i].appendChild(dotPara1)
             dotPara1.classList.add("dotPara")
         }
-        // else if (i == 30) {
-        //     let dotRotate = 0
-        //     dots[i].style.cssText = `transform: rotate(${dotRotate}deg)`
-        //     let dotPara = document.createElement('P')
-        //     let dotText = document.createTextNode('12')
-        //     dotPara.appendChild(dotText)
-        //     dots[i].appendChild(dotPara)
-        //     dotPara.classList.add("dotPara")
-        // }
-        // else if (i == 45) {
-        //     let dotRotate = 0
-        //     dots[i].style.cssText = `transform: rotate(${dotRotate}deg)`
-        //     let dotPara = document.createElement('P')
-        //     let dotText = document.createTextNode('12')
-        //     dotPara.appendChild(dotText)
-        //     dots[i].appendChild(dotPara)
-        //     dotPara.classList.add("dotPara")
-        // }
+        else if (i == 30) {
+            let dotRotate = 180
+            dots[i].style.cssText = `transform: rotate(${dotRotate}deg)`
+            let dotPara = document.createElement('P')
+            let dotText = document.createTextNode('6')
+            dotPara.style.cssText = `
+            transform: rotate(180deg);
+            right: 1px;
+            top: 10px;
+            `
+            dotPara.appendChild(dotText)
+            dots[i].appendChild(dotPara)
+            dotPara.classList.add("dotPara")
+        }
+        else if (i == 45) {
+            let dotRotate = 270
+            dots[i].style.cssText = `transform: rotate(${dotRotate}deg)`
+            let dotPara = document.createElement('P')
+            dotPara.style.cssText = `
+            transform: rotate(90deg);
+            left: 9px;
+            top: 3px;
+            `
+            let dotText = document.createTextNode('9')
+            dotPara.appendChild(dotText)
+            dots[i].appendChild(dotPara)
+            dotPara.classList.add("dotPara")
+        }
     }
 }
 dotss()
